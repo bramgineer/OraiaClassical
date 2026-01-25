dced82a5113ba9664342b913f908e3d6272d854c
Bram Brovold <bramgineer@proton.me>
2026-01-24 19:04:15 -0600

    Initial Commit

---
+++ OraiaClassical/OraiaClassicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OraiaClassicalApp.swift
+//  OraiaClassical
+//
+//  Created by verdi65 on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct OraiaClassicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
